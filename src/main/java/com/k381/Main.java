f26fbc22655e3e11976b07c1c4fe0507f866f164
LastHero91 <kam.06@mail.ru>
2018-10-13 13:09:14 +0300

    wdith param and args

--- src/main/java/com/k381/Main.java
+++ src/main/java/com/k381/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Arrays.stream(args).forEach(System.out::println );
+        Arrays.stream(args).forEach(System.out::println);
 
         System.out.println(System.getProperty("threadCount", "5"));
     }
