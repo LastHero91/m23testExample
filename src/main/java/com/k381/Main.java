b6fd63c08b50121859a13586cb1cbb4800caae6c
LastHero91 <kam.06@mail.ru>
2018-10-13 13:07:28 +0300

    width parametr and arguments

--- src/main/java/com/k381/Main.java
+++ src/main/java/com/k381/Main.java
@@ -1,7 +1,11 @@
 package com.k381;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Arrays.stream(args).forEach(System.out::println );
 
+        System.out.println(System.getProperty("threadCount", "5"));
     }
 }
